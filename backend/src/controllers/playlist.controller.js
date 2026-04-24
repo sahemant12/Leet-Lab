0872be9b096bacafaffc7726928a0e20b53c328c
Hemant Sah <hemantsah2912@gmail.com>
2026-04-24 22:32:26 +0530

    remove deletePlayList

--- backend/src/controllers/playlist.controller.js
+++ backend/src/controllers/playlist.controller.js
@@ -81,6 +81,5 @@ export const addProblemToPlaylist = async (req, res) => {
 };
 
 export const deletePlayList = async (req, res) => {
-  // deletePlayList
 };
 export const removeProblemFromPlaylist = async (req, res) => {};
